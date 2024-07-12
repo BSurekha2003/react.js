e95b7cf3065bc1b87a8621ce278d67623e02f767
BSurekha2003 <bsurekha596@gmail.com>
2024-07-12 06:41:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
+import RegistrationForm from './RegistrationForm';
+import "./App.css";
+
+function App() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleForm = () => {
+    setIsLogin(!isLogin);
+  };
+
+  return (
+    <div className="App">
+      <h1>{isLogin ? 'Login' : 'Register'}</h1>
+      {isLogin ? <LoginForm /> : <RegistrationForm />}
+      <button onClick={toggleForm}>
+        {isLogin ? 'Go to Registration' : 'Go to Login'}
+      </button>
+    </div>
+  );
+}
+
+export default App;
